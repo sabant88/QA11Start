bf1f81e3e5c9d6c94432d3446e9c5ed0b224d7f4
sabant88 <sabant88@mail.ru>
2022-01-12 00:06:40 +0200

    login

---
+++ src/test/java/login.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class login {
+    WebDriver wb;
+
+    @BeforeMethod
+    public void preCondition(){
+        //open https://contacts-app.tobbymarshall815.vercel.app/login
+    }
+
+    @Test
+    public void firstTest(){
+
+        //open form
+        //fill login correct value
+        //submit login
+
+    }
+
+    @Test
+    public void secondTest(){
+
+        //open form
+        //fill login incorrect value
+        //submit login
+
+    }
+    @AfterMethod
+    public void postCondition(){
+        //logout
+    }
+
+}
